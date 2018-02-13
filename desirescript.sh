9c077153eaae7825b0635abf7a9e205202c80952
yonseiyes <yonseiyes@gmail.com>
2018-02-13 18:11:02 +0900

    Create desirescript.sh

---
+++ desirescript.sh
@@ -0,0 +1,42 @@
+# Desire
+#!/bin/bash
+if free | awk '/^Swap:/ {exit !$2}'; then
+echo "Have swap"
+else
+sudo touch /var/swap.img
+sudo chmod 600 /var/swap.img
+sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
+sudo mkswap /var/swap.img
+sudo swapon /var/swap.img
+sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+fi
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get install nano htop git -y
+sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils software-properties-common -y
+sudo apt-get install libboost-all-dev -y
+sudo apt-get install libzmq3-dev libminiupnpc-dev libssl-dev libevent-dev -y
+sudo add-apt-repository ppa:bitcoin/bitcoin -y
+sudo apt-get update -y
+sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
+sudo git clone https://github.com/lazyboozer/Desire.git
+cd $HOME/Desire
+chmod -R 755 autogen.sh
+./autogen.sh
+./configure
+sudo chmod 755 share/genbuild.sh
+sudo make
+cd src
+./desired -daemon
+sleep
+yellow " Enter externalip"
+  read -p " > externalip : " externalip
+echo 
+#externalip=$externalip
+masternodekey=$(./desire-cli masternode genkey)
+./desire-cli stop
+echo -e "rpcuser=yonseiyes\nrpcpassword=yonseiyes72\nrpcallowip=127.0.0.1\nlisten=1\nserver=1\ndaemon=1\nrpcport=9918\nstaking=0\nexternalip=$externalip:9919\nmaxconnections=256\nmasternode=1\nmasternodeprivkey=$masternodekey" >> $HOME/.desirecore/desire.conf
+#./desired -daemon
+echo "Masternode private key: $masternodekey"
+echo "Job completed successfully"
